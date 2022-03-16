e386cfc14b436bdf2bf5af813704e34e0b11efea
Taoufiq Fatim Ouedraogo <taoufiq-fatim.ouedraogo@etu.utc.fr>
2022-03-16 14:20:20 +0000

    Update select_table_script.sql

--- Devoir 2 - TD3/select_table_script.sql
+++ Devoir 2 - TD3/select_table_script.sql
@@ -1,6 +1,9 @@
 --Nom et prénom du conducteur du bus immatriculé "5657 MA 49".
 SELECT nom, prenom FROM Conducteur, Bus WHERE  Conducteur.matricule = Bus.matricule AND Bus.immat = '5657 MA 49';
 
+-- Avec une JOINTURE
+SELECT c.nom, c.prenom FROM Conducteur as c JOIN Bus as b ON c.matricule=b.matricule where b.immat='5657 MA 49'
+
 --Immatriculation et type des bus qui partent de la station "Gare SNCF".
 
 --Numéros des lignes qui ne sont pas desservies par des bus à soufflets.
