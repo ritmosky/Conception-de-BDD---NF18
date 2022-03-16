826c7bb9beb40e504f181e2416f9a6323e81d852
Taoufiq Fatim Ouedraogo <taoufiq-fatim.ouedraogo@etu.utc.fr>
2022-03-16 11:36:02 +0000

    Update create_table_script.sql

--- Devoir 2 - TD3/create_table_script.sql
+++ Devoir 2 - TD3/create_table_script.sql
@@ -22,3 +22,26 @@ CREATE TABLE Conducteur (
     nom VARCHAR(50),
     prenom VARCHAR(50)
 );
+
+
+CREATE TABLE Bus (
+    immat VARCHAR(10),
+    type VARCHAR(10),
+    kilometrage NUMERIC(7),
+    matricule VARCHAR(10),
+
+    CHECK (type = 'Soufflets' or type='Normaux' or type='Reduits'),
+    PRIMARY KEY (immat),
+    FOREIGN KEY (matricule) REFERENCES Conducteur (matricule)
+);
+
+
+CREATE TABLE Trajet (
+    immat VARCHAR(10),
+    ligne INT,
+    nbtrajets NUMERIC(2),
+
+    PRIMARY KEY (ligne, immat),
+    FOREIGN KEY (ligne) REFERENCES Ligne (num),
+    FOREIGN KEY (immat) REFERENCES Bus (immat)
+);
