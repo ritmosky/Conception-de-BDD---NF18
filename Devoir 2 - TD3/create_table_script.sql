d921095de1ec7b0cca536d9eb9527458726d542c
Calliste Vergnol <calliste.vergnol@etu.utc.fr>
2022-03-16 11:28:22 +0000

    Add new file and create table inputs

---
+++ Devoir 2 - TD3/create_table_script.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Ligne (
+    num INTEGER PRIMARY KEY,
+    km INTEGER
+);
+
+CREATE TABLE Station (
+    num INTEGER PRIMARY KEY,
+    nom VARCHAR(255),
+    adresse VARCHAR(255)
+);
+
+CREATE TABLE Arret (
+    ligne INTEGER,
+    station INTEGER,
+    rang INTEGER,
+    FOREIGN KEY (ligne) REFERENCES Ligne(num),
+    FOREIGN KEY (station) REFERENCES Station(num)
+);
+
+CREATE TABLE Conducteur (
+    matricule VARCHAR(10) PRIMARY KEY,
+    nom VARCHAR(50),
+    prenom VARCHAR(50)
+);
