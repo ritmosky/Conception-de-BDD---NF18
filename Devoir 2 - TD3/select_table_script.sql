a35d90c5a0d31ebd66afe70337473729cdc88565
Calliste Vergnol <calliste.vergnol@etu.utc.fr>
2022-03-16 12:00:38 +0000

    Update select_table_script.sql

--- Devoir 2 - TD3/select_table_script.sql
+++ Devoir 2 - TD3/select_table_script.sql
@@ -1,4 +1,5 @@
 --Nom et prénom du conducteur du bus immatriculé "5657 MA 49".
+SELECT nom, prenom FROM Conducteur, Bus WHERE  Conducteur.matricule = Bus.matricule AND Bus.immat = '5657 MA 49';
 
 --Immatriculation et type des bus qui partent de la station "Gare SNCF".
 
