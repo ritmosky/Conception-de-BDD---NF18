7254beece1809ab2722a3a92d79d2dd97699d16e
Taoufiq Fatim Ouedraogo <taoufiq-fatim.ouedraogo@etu.utc.fr>
2022-03-16 15:15:57 +0000

    Update select_table_script.sql

--- Devoir 2 - TD3/select_table_script.sql
+++ Devoir 2 - TD3/select_table_script.sql
@@ -5,6 +5,11 @@ SELECT nom, prenom FROM Conducteur, Bus WHERE  Conducteur.matricule = Bus.matric
 SELECT c.nom, c.prenom FROM Conducteur as c JOIN Bus as b ON c.matricule=b.matricule where b.immat='5657 MA 49'
 
 --Immatriculation et type des bus qui partent de la station "Gare SNCF".
+SELECT immat, type FROM (Bus NATURAL JOIN Trajet) r, Arret a 
+NATURAL JOIN STATION s 
+WHERE s.nom='Gare SNCF' AND a.rang=1
+
+
 
 --Numéros des lignes qui ne sont pas desservies par des bus à soufflets.
 
