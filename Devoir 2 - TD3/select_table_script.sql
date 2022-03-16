95b2beba2f9278d4ccd7cb5d87a6d85eebf17021
Calliste Vergnol <calliste.vergnol@etu.utc.fr>
2022-03-16 11:52:17 +0000

    Add new file

---
+++ Devoir 2 - TD3/select_table_script.sql
@@ -0,0 +1,6 @@
+--Nom et prénom du conducteur du bus immatriculé "5657 MA 49".
+
+--Immatriculation et type des bus qui partent de la station "Gare SNCF".
+
+--Numéros des lignes qui ne sont pas desservies par des bus à soufflets.
+
