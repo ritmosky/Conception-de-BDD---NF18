bcf72386c9b8fce16bde6a26713f99a72a0ad5e9
Calliste Vergnol <calliste.vergnol@etu.utc.fr>
2022-04-27 08:40:15 +0000

    CREATE TABLE Asso_Compte_Client + correction NOT NULL in Client and Compte

--- Projet_NF18_Gestion_comptes_bancaires/create.sql
+++ Projet_NF18_Gestion_comptes_bancaires/create.sql
@@ -1,13 +1,19 @@
 CREATE TABLE Client (
     tel INTEGER, 
-    nom VARCHAR(25), 
-    adresse : VARCHAR(150), 
-    PRIMARY KEY(tel)
+    nom VARCHAR(25) NOT NULL, 
+    adresse : VARCHAR(150) NOT NULL, 
+    PRIMARY KEY (tel)
 );   
 
 CREATE TABLE Compte (
     date_crea DATETIME, 
-    balance FLOAT, 
-    statut VARCHAR(6) CHECK (statut='ouvert' OR statut='bloqué' OR statut='fermé'),
-    PRIMARY KEY(date_crea)
+    balance FLOAT NOT NULL, 
+    statut VARCHAR(6) CHECK (statut='ouvert' OR statut='bloqué' OR statut='fermé') NOT NULL,
+    PRIMARY KEY (date_crea)
+);
+
+CREATE TABLE Asso_Compte_Client (
+    tel INTEGER,
+    date_crea DATETIME,
+    FOREIGN KEY (tel, date_crea) REFERENCES Client(tel), Compte(date_crea)
 );
