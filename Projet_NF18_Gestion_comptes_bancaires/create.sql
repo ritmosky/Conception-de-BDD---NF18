0200c679a25d3c7292d932131bf3a49b69f81c7d
Calliste Vergnol <calliste.vergnol@etu.utc.fr>
2022-04-27 08:47:25 +0000

    TIMESTAMP ADDED + FOREIGN KEYS QUICK FIX

--- Projet_NF18_Gestion_comptes_bancaires/create.sql
+++ Projet_NF18_Gestion_comptes_bancaires/create.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Client (
     tel INTEGER, 
     nom VARCHAR(25) NOT NULL, 
-    adresse : VARCHAR(150) NOT NULL, 
+    adresse VARCHAR(150) NOT NULL, 
     PRIMARY KEY (tel)
 );   
 
 CREATE TABLE Compte (
-    date_crea DATETIME, 
+    date_crea TIMESTAMP, 
     balance FLOAT NOT NULL, 
     statut VARCHAR(6) CHECK (statut='ouvert' OR statut='bloqué' OR statut='fermé') NOT NULL,
     PRIMARY KEY (date_crea)
@@ -14,6 +14,8 @@ CREATE TABLE Compte (
 
 CREATE TABLE Asso_Compte_Client (
     tel INTEGER,
-    date_crea DATETIME,
-    FOREIGN KEY (tel, date_crea) REFERENCES Client(tel), Compte(date_crea)
+    date_crea TIMESTAMP,
+    PRIMARY KEY (tel, date_crea),
+    FOREIGN KEY (tel) REFERENCES Client(tel),
+    FOREIGN KEY (date_crea) REFERENCES Compte(date_crea)
 );
