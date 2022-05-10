e42cb3ed2843c632fa25efb329cbb0e9189b77ff
Taoufiq OUEDRAOGO <color.branch>
2022-05-10 12:02:57 +0200

    insert.sql modif

--- Projet_NF18_Gestion_comptes_bancaires/SQL_et_Data/insert.sql
+++ Projet_NF18_Gestion_comptes_bancaires/SQL_et_Data/insert.sql
@@ -15,11 +15,9 @@ INSERT INTO Client VALUES (0642112376, 'Hélène', '65 av de Notre-Dame');
 -- Alexis
 INSERT INTO Compte VALUES ('2021-05-03 06:14:00.75', 'ouvert');
 INSERT INTO CompteEpargne VALUES ('2021-05-03 06:14:00.75', 600.00, 300.00);
-
 INSERT INTO Compte VALUES ('2021-05-03 07:14:00.75', 'ouvert');
 INSERT INTO CompteCourant (date_crea, balance, montant_decouvert_autorise, max_solde, min_solde) 
 VALUES ('2021-05-03 07:14:00.75', 1156.00, 300.00, 1200.00, 300.00);
-
 INSERT INTO Asso_Compte_client VALUES (0665357620, '2021-05-03 06:14:00.75');
 INSERT INTO Asso_Compte_client VALUES (0665357620, '2021-05-03 07:14:00.75');
 
@@ -27,34 +25,28 @@ INSERT INTO Asso_Compte_client VALUES (0665357620, '2021-05-03 07:14:00.75');
 INSERT INTO Compte VALUES ('2021-05-03 07:16:00.75', 'ouvert');
 INSERT INTO CompteCourant (date_crea, balance, montant_decouvert_autorise, max_solde, min_solde) 
 VALUES ('2021-05-03 07:16:00.75', 35650.67, 1000.00, 45607.54, 30432.37);
-
 INSERT INTO Asso_Compte_client VALUES (0671307648, '2021-05-03 07:16:00.75');
 
 -- Maxime
 INSERT INTO Compte VALUES ('2022-02-08 06:14:00.75', 'ouvert');
 INSERT INTO CompteEpargne VALUES ('2022-02-08 06:14:00.75', 900.00, 100.00);
-
 INSERT INTO Compte VALUES ('2022-05-02 07:14:00.75', 'ouvert');
 INSERT INTO CompteCourant (date_crea, balance, montant_decouvert_autorise, max_solde, min_solde) 
 VALUES ('2022-05-02 07:14:00.75', 900.00, 300.00, 1200.00, 300.00);
-
 INSERT INTO Asso_Compte_client VALUES (0752418768, '2022-02-08 06:14:00.75');
 INSERT INTO Asso_Compte_client VALUES (0752418768, '2022-05-02 07:14:00.75');
 
 -- Mathieu
 INSERT INTO Compte VALUES ('2021-09-17 10:15:32.98', 'bloqué');
 INSERT INTO CompteCourant VALUES ('2021-09-17 10:15:32.98', 102.34, -300.00, 1300.00, 102.00, '2022-04-30');
-
 INSERT INTO Asso_Compte_client VALUES (0663768834, '2021-09-17 10:15:32.98');
 
 -- Jules
 INSERT INTO Compte VALUES ('2021-10-23 11:21:49.32', 'ouvert');
 INSERT INTO CompteCourant (date_crea, balance, montant_decouvert_autorise, max_solde, min_solde) 
 VALUES ('2021-10-23 11:21:49.32', 4677.68, 500.00, 5898.45, 1337.39);
-
 INSERT INTO Compte VALUES ('2021-11-24 11:21:49.32', 'ouvert');
 INSERT INTO CompteRevolving VALUES ('2021-11-24 11:21:49.32', -300.00, 0.001, -1000.00);
-
 INSERT INTO Asso_Compte_client VALUES (0764723098, '2021-10-23 11:21:49.32');
 INSERT INTO Asso_Compte_client VALUES (0764723098, '2021-11-24 11:21:49.32');
 
@@ -62,24 +54,20 @@ INSERT INTO Asso_Compte_client VALUES (0764723098, '2021-11-24 11:21:49.32');
 INSERT INTO Compte VALUES ('2019-09-09 09:09:09.09', 'fermé');
 INSERT INTO CompteCourant (date_crea, balance, montant_decouvert_autorise, max_solde, min_solde) 
 VALUES ('2019-09-09 09:09:09.09', 0.56, 100.00, 3298.43, 0.55);
-
 INSERT INTO Asso_Compte_client VALUES (0763357620, '2019-09-09 09:09:09.09');
 
 -- Alexandre
 INSERT INTO Compte VALUES ('2020-01-27 15:34:32.02', 'ouvert');
 INSERT INTO CompteCourant (date_crea, balance, montant_decouvert_autorise, max_solde, min_solde) 
 VALUES ('2020-01-27 15:34:32.02', 430.23, 100.00, 1112.38, 130.43);
-
 INSERT INTO Asso_Compte_client VALUES (0655298443, '2020-01-27 15:34:32.02');
 
 -- Paul
 INSERT INTO Compte VALUES ('2022-02-16 15:36:32.02', 'ouvert');
 INSERT INTO CompteCourant (date_crea, balance, montant_decouvert_autorise, max_solde, min_solde) 
 VALUES ('2022-02-16 15:36:32.02', 1230.54, 150.00, 1589.13, 143.56);
-
 INSERT INTO Compte VALUES ('2022-04-23 14:56:12.23', 'ouvert');
 INSERT INTO CompteEpargne VALUES ('2022-04-23 14:56:12.23', 650.00, 100.00);
-
 INSERT INTO Asso_Compte_client VALUES (0736542021, '2022-02-16 15:36:32.02');
 INSERT INTO Asso_Compte_client VALUES (0736542021, '2022-04-23 14:56:12.23');
 
@@ -87,17 +75,14 @@ INSERT INTO Asso_Compte_client VALUES (0736542021, '2022-04-23 14:56:12.23');
 INSERT INTO Compte VALUES ('2022-05-03 15:36:32.02', 'ouvert');
 INSERT INTO CompteCourant (date_crea, balance, montant_decouvert_autorise, max_solde, min_solde) 
 VALUES ('2022-05-03 15:36:32.02', 967.87, 150.00, 967.89, 10.00);
-
 INSERT INTO Asso_Compte_client VALUES (0776854098, '2022-05-03 15:36:32.02');
 
 -- Hélène
 INSERT INTO Compte VALUES ('2021-08-13 15:36:32.02', 'ouvert');
 INSERT INTO CompteCourant (date_crea, balance, montant_decouvert_autorise, max_solde, min_solde) 
 VALUES ('2021-08-13 15:36:32.02', 1267.87, 150.00, 1432.43, 300.43);
-
 INSERT INTO Compte VALUES ('2022-03-04 11:02:30.45', 'ouvert');
 INSERT INTO CompteRevolving VALUES ('2022-03-04 11:02:30.45', -210.45, 0.0015, -400.00);
-
 INSERT INTO Asso_Compte_client VALUES (0642112376, '2021-08-13 15:36:32.02');
 INSERT INTO Asso_Compte_client VALUES (0642112376, '2022-03-04 11:02:30.45');
 
@@ -105,7 +90,7 @@ INSERT INTO Asso_Compte_client VALUES (0642112376, '2022-03-04 11:02:30.45');
 INSERT INTO Operation VALUES (1, 300, '2022-05-09', 'non traité', 0665357620, '2021-05-03 06:14:00.75');
 INSERT INTO DebitGuichet VALUES (1);
 
---INSERT INTO Operation VALUES (2, 2000, '2022-05-09 12:30', 'non traité', 0665357620, '2021-05-03 06:14:00.75');
+--INSERT INTO Operation VALUES (2, 2000, '2022-05-09', 'non traité', 0665357620, '2021-05-03 06:14:00.75');
 --INSERT INTO EmissionCheque VALUES(2);
 
 INSERT INTO Operation VALUES (3, 200, '2022-05-06', 'non traité', 0671307648, '2021-05-03 07:16:00.75');
@@ -117,7 +102,7 @@ INSERT INTO Virement VALUES (4);
 INSERT INTO Operation VALUES (5, 230.00, '2022-05-07', 'non traité', 0764723098, '2021-10-23 11:21:49.32');
 INSERT INTO EmissionCheque VALUES (5);
 
--- INSERT INTO Operation VALUES (6, 1100.00, '2022-05-03', 'traité', 0764723098, '2021-10-23 11:21:49.32');
+-- INSERT INTO Operation VALUES (6, 1100.00, '2022-05-03', 'non traité', 0764723098, '2021-10-23 11:21:49.32');
  --INSERT INTO EmissionCheque VALUES (6); //erreur avec UNIQUE dans la création de table : on ne peut pas avoir deux opérations sur un compte
 
 INSERT INTO Operation VALUES (7, 100.00, '2022-05-03', 'traité', 0764723098, '2021-11-24 11:21:49.32');
